61bb24f755b3c6a2463f2a585e61448547822f30
Ch3mtr4ils <2775750552@qq.com>
2022-12-07 10:02:51 +0800

    Site updated: 2022-12-07 10:02:51

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4A86E8",
+      "tmpSize": "16",
+      "cityColor": "4A86E8",
+      "citySize": "16",
+      "aqiColor": "4A86E8",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "5px 2px 5px 2px",
+      "shadow": "1",
+      "language": "auto",
+      "borderRadius": "10",
+      "fixed": "false",
+      "vertical": "left",
+      "horizontal": "left",
+      "key": "2a989e47d2574a28883951ffc23f084f"
+    }
+  }
