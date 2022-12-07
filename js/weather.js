8e4473b065d85c3905660760332535b2a286f5a0
Ch3mtr4ils <2775750552@qq.com>
2022-12-07 20:31:56 +0800

    Site updated: 2022-12-07 20:31:56

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,21 @@
 WIDGET = {
-    "CONFIG": {
-      "modules": "01234",
-      "background": "5",
-      "tmpColor": "4A86E8",
-      "tmpSize": "16",
-      "cityColor": "4A86E8",
-      "citySize": "16",
-      "aqiColor": "4A86E8",
-      "aqiSize": "16",
-      "weatherIconSize": "24",
-      "alertIconSize": "18",
-      "padding": "5px 2px 5px 2px",
-      "shadow": "1",
-      "language": "auto",
-      "borderRadius": "10",
-      "fixed": "false",
-      "vertical": "left",
-      "horizontal": "left",
-      "key": "2a989e47d2574a28883951ffc23f084f"
-    }
+  "CONFIG": {
+    "modules": "0123",
+    "background": "5",
+    "tmpColor": "4A86E8",
+    "tmpSize": "16",
+    "cityColor": "4A86E8",
+    "citySize": "16",
+    "aqiColor": "4A86E8",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "0c8558e33bbb4302bff60b1187569f12"
   }
+}
