02de54109f80292942b5900c27673e828679e439
Ch3mtr4ils <2775750552@qq.com>
2022-12-23 10:58:46 +0800

    Site updated: 2022-12-23 10:58:45

--- js/xxx.js
+++
@@ -1,6 +0,0 @@
-// 分享本页
-function share() {
-    let url = window.location.origin + window.location.pathname
-    new ClipboardJS(".share", { text: function() { return '标题：' + document.title + '\n链接：' + url } });
-    btf.snackbarShow("本页链接已复制到剪切板，快去分享吧~")
-}
